4c0494e37717450864088990ecb15802fc1d78c8
Ari Souza <79428565+arimateia-souza@users.noreply.github.com>
2022-06-23 09:58:29 -0300

    first commit

---
+++ vendas.py
@@ -0,0 +1,16 @@
+from Funcoes import *
+
+def principal():
+    while(True):
+        print("Bem-vindo ao ...\n")
+        print("(1) Cadastrar produto")
+        print("(2) Cadastrar produto")
+        print("(3) Cadastrar produto")
+        print("(4) Cadastrar produto\n")
+        opcao=int(input("Escolha a opção desejada: "))
+        if (opcao == 1):
+            cadastrar_produto()
+
+
+
+principal()
