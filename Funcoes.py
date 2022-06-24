eb8df9479c3b5bce902532da074f1a885b43a625
Ari Souza <email.arisantos@gmail.com>
2022-06-23 21:19:08 -0300

    second commit

--- Funcoes.py
+++ Funcoes.py
@@ -1,2 +1,22 @@
 def cadastrar_produto():
-    print("test")
+    print("Você selecionou cadastrar um produto.")
+    produtos =()
+    nome = input("Digite o nome do produto: ")
+    preco = input("Digite o preço do produto: ")
+    quantidade = input("Digite a quantidade do produto: ")
+    produtos.append(nome, preco, quantidade)
+    return produtos
+
+#def ver_quantidade_produto():
+
+
+#def listar_produtos():
+
+
+#def vender_produto():
+
+
+#def repor_produto(produtos):
+    #produtos.append()
+#def historico_vendas():
+
--- vendas.py
+++ vendas.py
@@ -4,12 +4,28 @@ def principal():
     while(True):
         print("Bem-vindo ao ...\n")
         print("(1) Cadastrar produto")
-        print("(2) Cadastrar produto")
-        print("(3) Cadastrar produto")
-        print("(4) Cadastrar produto\n")
+        print("(2) Ver quantidade de produtos")
+        print("(3) Listar produtos")
+        print("(4) Vender um produto")
+        print("(5) Repor produto")
+        print("(6) Historico de vendas\n")
+
         opcao=int(input("Escolha a opção desejada: "))
+
         if (opcao == 1):
             cadastrar_produto()
+        elif(opcao == 2):
+            ver_quantidade_produto()
+        elif(opcao == 3):
+            listar_produtos()
+        elif(opcao == 4):
+            vender_produto()
+        elif(opcao == 5):
+            repor_produto()
+        elif(opcao == 6):
+            historico_vendas()
+        else: print("opção invalida, tente novamente!")
+
 
 
 
