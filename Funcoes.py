55b2c181d57e8f3562f9ca0789c1699a69dae6d6
Ari Souza <email.arisantos@gmail.com>
2022-06-29 23:08:43 -0300

    fourth commit

--- Funcoes.py
+++ Funcoes.py
@@ -1,36 +1,15 @@
-produtos = []
-def principal():
-    print("Bem-vindo ao ...!")
-    while(True):
 
-        print("(1) Cadastrar produto")
-        print("(2) Ver quantidade de produtos")
-        print("(3) Listar produtos")
-        print("(4) Vender um produto")
-        print("(5) Repor produto")
-        print("(6) Historico de vendas")
-        opcao=int(input("Escolha a opção desejada: "))
-        if (opcao == 1):
-            cadastrar_produto()
-        elif(opcao == 2):
-            ver_quantidade_produto()
-        elif(opcao == 3):
-            listar_produtos()
-        elif(opcao == 4):
-            vender_produto()
-        elif(opcao == 5):
-            repor_produto()
-        elif(opcao == 6):
-            historico_vendas()
-        else: print("opção invalida, tente novamente!")
 
-def cadastrar_produto(produtos):
+def cadastrar_produto():
     print("Você selecionou cadastrar um produto.")
     nome = input("Digite o nome do produto: ")
     preco = input("Digite o preço do produto: ")
     quantidade = input("Digite a quantidade do produto: ")
-    produtos = [(nome), (preco), (quantidade)]
-    return produtos
+    produto = ((nome, preco, quantidade))
+    return produto
+
+
+
 
 def ver_quantidade_produto():
     print("entrou na funçao quantidade")
@@ -49,4 +28,3 @@ def historico_vendas():
 
 
 
-cadastrar_produto(produtos)
--- vendas.py
+++ vendas.py
@@ -1,3 +1,30 @@
 from Funcoes import *
 
+def principal():
+    produto = []
+    produto.append(cadastrar_produto())
+    print("Bem-vindo ao ...!")
+    while(True):
+
+
+        print("(1) Cadastrar produto")
+        print("(2) Ver quantidade de produtos")
+        print("(3) Listar produtos")
+        print("(4) Vender um produto")
+        print("(5) Repor produto")
+        print("(6) Historico de vendas")
+        opcao=int(input("Escolha a opção desejada: "))
+        if (opcao == 1):
+            cadastrar_produto(produto)
+        elif(opcao == 2):
+            ver_quantidade_produto()
+        elif(opcao == 3):
+            listar_produtos()
+        elif(opcao == 4):
+            vender_produto()
+        elif(opcao == 5):
+            repor_produto()
+        elif(opcao == 6):
+            historico_vendas()
+        else: print("opção invalida, tente novamente!")
 principal()
